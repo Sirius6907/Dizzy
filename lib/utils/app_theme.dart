98ab1724cd891204931ed45fcb745ad9a0aab340
Sirius6907 <kumarchandan12345a@gmail.com>
2026-09-04 13:11:52 +0530

    v1.1.1: Add Dizzy component library and begin migrating home_screen.dart

--- lib/utils/app_theme.dart
+++ lib/utils/app_theme.dart
@@ -156,6 +156,18 @@ class AppTheme {
   static BoxDecoration get effectiveBackground =>
       isLightMode ? backgroundDecorationFlat : backgroundDecoration;
 
+  /// Spacing & Border Radius Design Tokens
+  static const double spaceXS = 4.0;
+  static const double spaceS = 8.0;
+  static const double spaceM = 16.0;
+  static const double spaceL = 24.0;
+  static const double spaceXL = 32.0;
+
+  static const double radiusS = 8.0;
+  static const double radiusM = 12.0;
+  static const double radiusL = 16.0;
+  static const double radiusXL = 24.0;
+
   static ThemeData get themeData {
     final preset = current;
     return ThemeData(
